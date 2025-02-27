8bc623b9ff22a972b8c04d278eebe4d0ad3dcf93
primeslx <lucio.diego4002@gmail.com>
2025-02-27 08:16:10 -0300

    dump

--- dump-db.sh
+++ dump-db.sh
@@ -1,3 +1,7 @@
-DUMP-FILE="database/dump.sql"
+DUMP-FILE="./database/dump.sql"
 
-./vendor/bin/sail exec mysql mysqldump -u root -p"password" --all-databases > backup.sql
+./vendor/bin/sail exec mysql mysqldump -u root -p"password" --all-databases > ./database/dump.sql
+
+git add "database/dump.sql"
+
+echo "📌 Dump do banco salvo e adicionado ao commit!"
