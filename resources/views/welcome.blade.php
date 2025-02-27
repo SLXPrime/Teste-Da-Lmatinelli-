e533e1b0e5e4698085791bf4dc7c241bf28404c5
primeslx <lucio.diego4002@gmail.com>
2025-02-26 21:42:26 -0300

    epa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                             </div>
 
                             <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                <div class="mt-2  text-gray-600 dark:text-gray-400 text-sm">
                                     é nada
                                 </div>
                             </div>
